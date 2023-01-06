d70d325a1441b30cfd5316e810f4d6c40395c52c
Tim203 <mctim203@gmail.com>
2023-01-06 11:03:16 +0100

    Run isSnapshot after evaluation

--- build-logic/src/main/kotlin/api.publish-conventions.gradle.kts
+++ build-logic/src/main/kotlin/api.publish-conventions.gradle.kts
@@ -13,18 +13,22 @@ publishing {
     }
 }
 
-artifactory {
-    setContextUrl("https://repo.opencollab.dev/artifactory")
-    publish {
-        repository {
-            setRepoKey(if (isSnapshot()) "maven-snapshots" else "maven-releases")
-            setMavenCompatible(true)
-        }
-        defaults {
-            publications("mavenJava")
-            setPublishArtifacts(true)
-            setPublishPom(true)
-            setPublishIvy(false)
+// without afterEvaluate isSnapshot would always return false
+// because the version wouldn't have been set yet
+afterEvaluate {
+    artifactory {
+        setContextUrl("https://repo.opencollab.dev/artifactory")
+        publish {
+            repository {
+                setRepoKey(if (isSnapshot()) "maven-snapshots" else "maven-releases")
+                setMavenCompatible(true)
+            }
+            defaults {
+                publications("mavenJava")
+                setPublishArtifacts(true)
+                setPublishPom(true)
+                setPublishIvy(false)
+            }
         }
     }
 }
