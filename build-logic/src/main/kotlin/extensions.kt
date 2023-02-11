bac896a173abbc1426d33be869bbbb45a37f7676
Tim203 <mctim203@gmail.com>
2023-02-11 13:32:21 +0100

    cherrypick changes

--- build-logic/src/main/kotlin/extensions.kt
+++ build-logic/src/main/kotlin/extensions.kt
@@ -1,4 +1,15 @@
+import net.kyori.indra.git.IndraGitExtension
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.the
 
 fun Project.isSnapshot(): Boolean =
     version.toString().endsWith("-SNAPSHOT")
+
+fun Project.branchName(): String =
+    the<IndraGitExtension>().branchName() ?: System.getenv("BRANCH_NAME") ?: "local/dev"
+
+fun Project.shouldAddBranchName(): Boolean =
+    System.getenv("IGNORE_BRANCH")?.toBoolean() ?: (branchName() !in arrayOf("master", "local/dev"))
+
+fun Project.versionWithBranchName(): String =
+    branchName().replace(Regex("[^0-9A-Za-z-_]"), "-") + '-' + version
