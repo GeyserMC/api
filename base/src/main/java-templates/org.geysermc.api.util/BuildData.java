f2f9a04c0ba910c9d1372920a7c3ae5473bcf4ce
onebeastchris <github@onechris.mozmail.com>
2024-07-13 20:46:24 +0200

    Move BuildData to util

--- base/src/main/java-templates/org.geysermc.api.util/BuildData.java
+++ base/src/main/java-templates/org.geysermc.api.util/BuildData.java
@@ -23,7 +23,7 @@
  * @link https://github.com/GeyserMC/Geyser
  */
 
-package org.geysermc.api;
+package org.geysermc.api.util;
 
 // This constant is replaced before compilation
 public class BuildData {
