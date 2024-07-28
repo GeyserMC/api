baa07f6f5f04716bd2016c9ac76f5546aff3dccd
chris <github@onechris.mozmail.com>
2024-07-28 19:36:56 +0200

    Update build-logic/src/main/kotlin/extensions.kt

--- build-logic/src/main/kotlin/extensions.kt
+++ build-logic/src/main/kotlin/extensions.kt
@@ -6,7 +6,7 @@ fun Project.branchName(): String =
     the<IndraGitExtension>().branchName() ?: System.getenv("BRANCH_NAME") ?: "local/dev"
 
 fun Project.shouldAddBranchName(): Boolean =
-    System.getenv("IGNORE_BRANCH")?.toBoolean() ?: (branchName() !in arrayOf("master", "local/dev"))
+    branchName() !in arrayOf("master", "main", "local/dev")
 
 fun Project.versionWithBranchName(): String =
     branchName().replace(Regex("[^0-9A-Za-z-_]"), "-") + '-' + version
