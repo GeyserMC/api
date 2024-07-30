8cc52f402cee3a1929e76ec03aaa37c9ec94cd62
onebeastchris <github@onechris.mozmail.com>
2024-07-31 00:10:39 +0200

    Don't add extra branchname to api version branches, dont upload files for the release action

--- build-logic/src/main/kotlin/extensions.kt
+++ build-logic/src/main/kotlin/extensions.kt
@@ -5,8 +5,8 @@ import org.gradle.kotlin.dsl.the
 fun Project.branchName(): String =
     the<IndraGitExtension>().branchName() ?: System.getenv("BRANCH_NAME") ?: "local/dev"
 
-fun Project.shouldAddBranchName(): Boolean =
-    branchName() !in arrayOf("master", "main", "local/dev")
+fun Project.shouldAddBranchName(version: String): Boolean =
+    branchName() !in arrayOf("master", "main", "local/dev", "api/$version")
 
 fun Project.versionWithBranchName(): String =
     branchName().replace(Regex("[^0-9A-Za-z-_]"), "-") + '-' + version
