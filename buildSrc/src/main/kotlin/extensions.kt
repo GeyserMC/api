184cd0beed4464db80a88d9dd234e3ce6eb23fca
onebeastchris <github@onechris.mozmail.com>
2026-05-11 21:41:39 +0200

    Bump indra

--- buildSrc/src/main/kotlin/extensions.kt
+++ buildSrc/src/main/kotlin/extensions.kt
@@ -3,10 +3,10 @@ import org.gradle.api.Project
 import org.gradle.kotlin.dsl.the
 
 fun Project.branchName(): String =
-  the<IndraGitExtension>().branchName() ?: System.getenv("BRANCH_NAME") ?: "local/dev"
+  the<IndraGitExtension>().branchName().getOrElse(System.getenv("BRANCH_NAME") ?: "local/dev")
 
-fun Project.shouldAddBranchName(versionString: String): Boolean {
-  val version = versionString.removeSuffix("-SNAPSHOT")
+fun Project.shouldAddBranchName(versionString: Any): Boolean {
+  val version = versionString.toString().removeSuffix("-SNAPSHOT")
   return branchName() !in arrayOf("master", "main", "local/dev", "api/$version")
 }
 
